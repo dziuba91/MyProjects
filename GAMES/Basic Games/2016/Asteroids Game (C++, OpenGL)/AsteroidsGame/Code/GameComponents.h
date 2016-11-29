9bf9d849bc6879aaf892b3c7ff98c3d1083ede68
Tomasz Dziuba <kira.dziuba333@gmail.com>
2016-11-29 18:45:24 +0100

    Games: clean old version

--- GAMES/Basic Games/2016/Asteroids Game (C++, OpenGL)/AsteroidsGame/Code/GameComponents.h
+++
@@ -1,8 +0,0 @@
-#ifndef __GAMECOMPONENTS_H__
-#define __GAMECOMPONENTS_H__
-
-#include "GameComponents/PlayerComponent.h"
-#include "GameComponents/ShootComponent.h"
-#include "GameComponents/AsteroidsComponent.h"
-
-#endif // __GAMECOMPONENTS_H__
